18f8fb88ae52383eb662d30435c96b0227edbcbd
Milan Dierick <milan.dierick@gmail.com>
2021-02-12 19:27:15 +0100

    REF: Default destructors

--- Heirloom/src/Heirloom/Renderer/Buffer.h
+++ Heirloom/src/Heirloom/Renderer/Buffer.h
@@ -122,7 +122,7 @@ namespace Heirloom
 	class VertexBuffer
 	{
 	public:
-		virtual ~VertexBuffer() {}
+		virtual ~VertexBuffer() = default;
 
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
@@ -136,7 +136,7 @@ namespace Heirloom
 	class IndexBuffer
 	{
 	public:
-		virtual ~IndexBuffer() {}
+		virtual ~IndexBuffer() = default;
 
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
